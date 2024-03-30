b9e75e6bfb92f38d493cf50fe5721df089166e56
yubinmimi <yubinmimi>
2024-03-30 14:14:12 +0000

    spell check algorithm developed

--- ingest_optouts.py
+++ ingest_optouts.py
@@ -1,4 +1,17 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Ingest the Opt-Out Affidavits
+# MAGIC
+# MAGIC The input files have a lot of errors (typos, etc.) and duplicates. It needs some amount of cleaning work. 
+# MAGIC We first load the data as-is, and then clean up the file a bit. 
+# MAGIC Also, the files are relatively small - loadable in memory, so we leverage that side to make things easy.
+
+# COMMAND ----------
+
+!pip install jellyfish
+
+# COMMAND ----------
+
 from pathlib import Path
 import re
 import csv
@@ -6,6 +19,9 @@ from pyspark.sql.types import StructType, StructField, StringType, DateType
 from pyspark.sql.functions import col, lit, to_date
 from datetime import datetime
 from dateutil.parser import parse
+import pandas as pd
+from jellyfish import damerau_levenshtein_distance as dld
+from jellyfish import soundex as sdx
 
 path = "/Volumes/mimi_ws_1/datacmsgov/src" # where all the input files are located
 catalog = "mimi_ws_1" # delta table destination catalog
@@ -23,64 +39,75 @@ files = sorted(files, key=lambda x: x[0], reverse=True)
 
 # COMMAND ----------
 
-files_exist = {}
-# We want to skip those files that are already in the delta tables.
-# We look up the table, and see if the files are already there or not.
-if spark.catalog.tableExists(f"{catalog}.{schema}.{tablename}"):
-    files_exist = set([row["_input_file_date"] 
-                   for row in 
-                   (spark.read.table(f"{catalog}.{schema}.{tablename}")
-                            .select("_input_file_date")
-                            .distinct()
-                            .collect())])
-files_to_ingest = [item for item in files if item[0] not in files_exist]
+def change_header(header_org):
+    return [re.sub(r'\W+', '', column.lower().replace(' ','_'))
+            for column in header_org]
+
+pdf_lst = []
+for item in files:
+    pdf = pd.read_csv(item[1])
+    pdf.columns = change_header(pdf.columns)
+    pdf["optout_effective_date"] = pd.to_datetime(pdf["optout_effective_date"]).dt.date
+    pdf["optout_end_date"] = pd.to_datetime(pdf["optout_end_date"]).dt.date
+    pdf["last_updated"] = pd.to_datetime(pdf["last_updated"]).dt.date
+    pdf = pdf.sort_values(by=["last_updated", "optout_end_date"], 
+                            ascending=False)
+    pdf = pdf.drop_duplicates(subset=["npi"], keep="first")
+    pdf["_input_file_date"] = item[0]
+    pdf_lst.append(pdf)
+pdf_full = pd.concat(pdf_lst)
 
 # COMMAND ----------
 
-header = []
-if len(files) > 0:
-    for column in next(csv.reader(open(str(files[0][1]), "r"))):
-        col_new = re.sub(r'\W+', '', column.lower().replace(' ','_'))
-        if col_new[-5:] == "_date":
-            header.append(StructField(col_new + "_", StringType(), True))
-        elif col_new == "last_updated":
-            header.append(StructField(col_new+"_date_", StringType(), True))
-        else:
-            header.append(StructField(col_new, StringType(), True))
-    #header.append(StructField("_input_file_date", DateType(), True))
-table_schema = StructType(header)
+word_cnt = {}
+for index, row in pdf_full.groupby('specialty').count().loc[:,["npi"]].iterrows():
+    word_cnt[index] = row['npi']
 
 # COMMAND ----------
 
-header
+# MAGIC %md
+# MAGIC ## Spell Correction for Medicine!
 
 # COMMAND ----------
 
-writemode = "overwrite"
-for item in sorted(files_to_ingest, key=lambda x: x, reverse=True):
-    df = (spark.read.format("csv")
-            .option("header", "false")
-            .option("skipRows", "1")
-            .schema(table_schema)
-            .load(str(item[1]))
-            .withColumn('_input_file_date', lit(item[0]))
-            .withColumn('optout_effective_date', 
-                        to_date(col('optout_effective_date_'), 
-                                'MM/dd/yyyy'))
-            .withColumn('optout_end_date', 
-                        to_date(col('optout_end_date_'), 
-                                'MM/dd/yyyy'))
-            .withColumn('last_updated_date', 
-                        to_date(col('last_updated_date_'), 
-                                'MM/dd/yyyy'))
-            .drop('optout_effective_date_', 'optout_end_date_', 'last_updated_date_'))
-    
-    if spark.catalog.tableExists(f"{catalog}.{schema}.{tablename}"):
-        writemode = "append"
-    
-    (df.write
-        .format('delta')
-        .mode(writemode)
+mapping = {}
+mapping_reverse = {}
+for word_a, score_a in word_cnt.items():
+    for word_b, score_b in word_cnt.items():
+        if word_a == word_b:
+            continue
+        distance = dld(word_a, word_b)
+        if (distance < 3 and 
+            score_a > score_b and 
+            sdx(word_a) == sdx(word_b)):
+            if ((word_b not in mapping) or
+                (score_a > mapping[word_b]["score_a"])):
+                doc = {"a": word_a,
+                        "b": word_b,
+                        "score_a": score_a,
+                        "score_b": score_b}
+                mapping[word_b] = doc
+                mapping_reverse[word_a] = doc
+
+for key_reverse, doc_reverse in mapping_reverse.items():
+    if key_reverse in mapping:
+        word_a = doc_reverse["a"]
+        word_b = doc_reverse["b"]
+        mapping[word_b]["a"] = mapping[word_a]["a"]
+        mapping[word_b]["score_a"] = mapping[word_a]["score_a"]
+
+replace_map = {k:k for k in word_cnt.keys()}
+replace_map = {**replace_map, **{k:v["a"] for k, v in mapping.items()}}
+
+# COMMAND ----------
+
+pdf_full["specialty"] = pdf_full["specialty"].map(replace_map)
+
+# COMMAND ----------
+
+(spark.createDataFrame(pdf_full).write
+        .format("delta")
+        .mode("overwrite")
         .saveAsTable(f"{catalog}.{schema}.{tablename}"))
 
 # COMMAND ----------
